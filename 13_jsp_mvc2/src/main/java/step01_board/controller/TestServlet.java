99ee89beb44843b68498552e7d51c9d24531c25e
kgh-codingWorld <tto4028@naver.com>
2024-07-25 17:16:36 +0900

    TestServlet 및 그외 셋팅파일 추가

---
+++ 13_jsp_mvc2/src/main/java/step01_board/controller/TestServlet.java
@@ -0,0 +1,16 @@
+package step01_board.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/test")
+public class TestServlet extends HttpServlet {
+	
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().append("Test Servlet is working");
+    }
+}
